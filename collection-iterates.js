9542046c6d31dd6b8a556a9c7011ec40e51e23d3
krist yohan <massashok420@gmail.com>
2020-08-03 04:47:17 +0530

    for in || for of loop added

--- collection-iterates.js
+++ collection-iterates.js
@@ -4,3 +4,24 @@ const name = [1, 2, 3, 4, 5];
 name.forEach((element, i) => {
   console.log("forEach loop:", element, i);
 });
+
+// for in loop
+
+obj = {
+  name: "ashok",
+  age: 23,
+};
+
+for (let x in obj) {
+  console.log(x);
+  console.log(obj[x]);
+}
+// using array
+for (let x in name) {
+  console.log(x);
+}
+
+// for of loop
+for (let x of name) {
+  console.log(x);
+}
